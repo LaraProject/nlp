d3f5c125616d018675260a37cbc968b87dc142b8
Louis Popi <theh2o64@gmail.com>
2020-05-21 11:10:17 +0200

    nlp: context: facebook: Set min_length=0 and max_length=40

--- src/main/java/org/lara/nlp/context/FacebookTest.java
+++ src/main/java/org/lara/nlp/context/FacebookTest.java
@@ -5,10 +5,10 @@ import org.lara.nlp.context.Facebook;
 class FacebookTest {
 	public static void main(String[] args) throws Exception {
 		// Facebook data
-		Facebook context = new Facebook(args[0], args[1], 5, 30);
+		Facebook context = new Facebook(args[0], args[1], 0, 40);
 		System.out.println("FacebookTest: json_filename = " + args[0]);
 		System.out.println("FacebookTest: answerer = " + args[1]);
-		System.out.println("FacebookTest: min_length = 5 | max_length = 30");
+		System.out.println("FacebookTest: min_length = 0 | max_length = 40");
 		System.out.println("FacebookTest: initializing...");
 		context.init();
 		System.out.println("FacebookTest: cleaning text...");
